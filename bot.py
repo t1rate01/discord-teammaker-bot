cb1230d07f30ca81333c122f77ee0b48b61705a1
Tero Rantanen <t1rate01@students.oamk.fi>
2023-02-25 11:12:10 +0200

    Fix

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
-PORT = int(os.environ.get('PORT', '5000'))
+PORT = int(os.environ.get('PORT', '8443'))
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -49,13 +49,11 @@ def main():
     # log all errors
     dp.add_error_handler(error)
 
-    # Start the Bot
-    updater.start_webhook(
-        listen="0.0.0.0",
-        port=int(PORT),
-        url_path=TOKEN,
-        webhook_url=heroku_url + TOKEN
-    )
+     # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+    port=int(PORT),
+    url_path=TOKEN,
+    webhook_url='https://telegram-kojamo-bot.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
