fad59cee1ad72be7efddd90e3813b04fa45dd0cf
Tero Rantanen <t1rate01@students.oamk.fi>
2023-02-25 11:47:48 +0200

    fix

--- bot.py
+++ bot.py
@@ -35,6 +35,7 @@ def main():
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
     updater = Updater(TOKEN, use_context=True)
+    
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
